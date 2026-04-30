bede9123e028f75d3e6314bea32c7cba3a0936a5
lobanovamc <lobanova.mc@gmail.com>
2026-04-30 15:15:56 -0700

    renamed

--- 0430/List-1>first_last6.py
+++
@@ -1,9 +0,0 @@
-def first_last6(nums):
-  if nums[len(nums)-1] == 6 or nums[0] == 6:
-    return True
-  else:
-    return False
-  
-first_last6([1, 2, 6])
-first_last6([6, 1, 2, 3])
-first_last6([13, 6, 1, 2, 3]) 

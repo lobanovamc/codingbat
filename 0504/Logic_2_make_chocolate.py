3b72a880a1dda7aa49feab594eb0cb80079ff589
lobanovamc <lobanova.mc@gmail.com>
2026-05-04 23:42:22 -0700

    Create Logic_2_make_chocolate.py

---
+++ 0504/Logic_2_make_chocolate.py
@@ -0,0 +1,16 @@
+# We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+
+# make_chocolate(4, 1, 9) → 4
+# make_chocolate(4, 1, 10) → -1
+# make_chocolate(4, 1, 7) → 2
+
+def make_chocolate(small, big, goal):
+    
+    big_n = min(big , goal // 5)
+
+    if goal / 5 <= big and goal % 5 <= small:
+        return goal % 5
+    if (goal - 5 * big_n) <= small:
+        return goal - big * 5
+  
+    return -1
